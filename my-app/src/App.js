03703d7520a8da82b4b585d68e87072c3239abe4
gondawara-yumeko <gondawara-yumeko@users.noreply.github.com>
2021-05-26 00:26:54 +0900

    Social sign-in (OAuth)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// ref: https://docs.amplify.aws/lib/auth/social/q/platform/js
 
-function App() {
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import awsConfig from './awsConfig';
+
+Amplify.configure(awsConfig);
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => {
+            setUser(userData);
+          });
+
+          break;
+        case 'signOut':
+          setUser(null);
+
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+
+          break;
+        default:
+          break;
+      }
+    });
+  }, []);
+
+  const getUser = () => {
+    return Auth.currentAuthenticatedUser().then(userData => {
+      return userData;
+    }).catch(() => {
+      console.log('Not signed in');
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>
+        User: {user ? JSON.stringify(user.attributes) : 'None'}
+      </p>
+
+      {user ? (
+        <button onClick={() => {
+          Auth.signOut();
+        }}>
+          Sign Out
+        </button>
+      ) : (
+        <button onClick={() => {
+          Auth.federatedSignIn();
+        }}>
+          Federated Sign In
+        </button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
