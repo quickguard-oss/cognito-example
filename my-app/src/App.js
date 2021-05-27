43ff14967a4801b2063d577b87f66f3f98999eeb
gondawara-yumeko <gondawara-yumeko@users.noreply.github.com>
2021-05-28 00:34:43 +0900

    Enable MFA

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,30 @@
-// ref: https://docs.amplify.aws/lib/auth/social/q/platform/js
+// refs:
+//   * https://docs.amplify.aws/lib/auth/social/q/platform/js
+//   * https://docs.amplify.aws/lib/auth/mfa/q/platform/js
 
 import Amplify, { Auth, Hub } from 'aws-amplify';
+import QRCode from 'qrcode.react';
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import awsConfig from './awsConfig';
 
 Amplify.configure(awsConfig);
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [qrCode, setQrCode] = useState(null);
 
   useEffect(() => {
-    Hub.listen('auth', ({ payload: { event, data } }) => {
+    Hub.listen('auth', async ({ payload: { event, data } }) => {
       switch (event) {
         case 'signIn':
         case 'cognitoHostedUI':
-          getUser().then(userData => {
+          try {
+            const userData = await Auth.currentAuthenticatedUser({ bypassCache: true });
+
             setUser(userData);
-          });
+          } catch (e) {
+            console.log('Not signed in');
+          }
 
           break;
         case 'signOut':
@@ -35,34 +42,165 @@ const App = () => {
     });
   }, []);
 
-  const getUser = () => {
-    return Auth.currentAuthenticatedUser().then(userData => {
-      return userData;
-    }).catch(() => {
-      console.log('Not signed in');
-    });
-  };
-
   return (
-    <div className='App'>
+    <div style={{
+      textAlign: 'center'
+    }}>
       <p>
-        User: {user ? JSON.stringify(user.attributes) : 'None'}
+        User: {user ? user.attributes.email : 'None'}
       </p>
 
       {user ? (
-        <button onClick={() => {
-          Auth.signOut();
-        }}>
-          Sign Out
-        </button>
+        qrCode ? (
+          <>
+            <TotpSetupForm
+              qrCode={qrCode}
+              setQrCode={setQrCode}
+              user={user}
+              setUser={setUser}
+            />
+
+            <SignOutButton />
+          </>
+        ) : (
+          user.preferredMFA !== 'NOMFA' ? (
+            <>
+              <CancelMfaButton
+                user={user}
+                setUser={setUser}
+              />
+
+              <SignOutButton />
+            </>
+          ) : (
+            <>
+              <SetupTotpButton
+                user={user}
+                setQrCode={setQrCode}
+              />
+
+              <SignOutButton />
+            </>
+          )
+        )
       ) : (
-        <button onClick={() => {
-          Auth.federatedSignIn();
-        }}>
-          Federated Sign In
-        </button>
+        <SignInButton />
       )}
-    </div>
+    </div >
+  );
+};
+
+const CancelMfaButton = (props) => {
+  const handleClick = async () => {
+    await Auth.setPreferredMFA(props.user, 'NOMFA');
+
+    const userData = await Auth.currentAuthenticatedUser({ bypassCache: true });
+
+    props.setUser(userData);
+  };
+
+  return (
+    <button onClick={() => {
+      handleClick();
+    }}>
+      Cancel MFA
+    </button>
+  );
+};
+
+const SetupTotpButton = (props) => {
+  const setupTotp = async () => {
+    const code = await Auth.setupTOTP(props.user);
+
+    props.setQrCode(
+      `otpauth://totp/AWSCognito:${props.user.username}?secret=${code}&issuer=${encodeURI('AWSCognito')}`
+    );
+  };
+
+  return (
+    <button onClick={() => {
+      setupTotp();
+    }}>
+      Setup TOTP
+    </button>
+  );
+};
+
+const SignOutButton = () => {
+  return (
+    <button
+      onClick={() => {
+        Auth.signOut();
+      }}
+    >
+      Sign Out
+    </button>
+  );
+};
+
+const SignInButton = () => {
+  return (
+    <button onClick={() => {
+      Auth.federatedSignIn();
+    }}>
+      Federated Sign In
+    </button>
+  );
+};
+
+const TotpSetupForm = (props) => {
+  const [oneTimePassword, setOneTimePassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await Auth.verifyTotpToken(props.user, oneTimePassword);
+
+      await Auth.setPreferredMFA(props.user, 'TOTP');
+
+      const userData = await Auth.currentAuthenticatedUser({ bypassCache: true });
+
+      props.setUser(userData);
+      props.setQrCode(null);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    setOneTimePassword(event.target.value);
+  };
+
+  return (
+    <>
+      <h2>
+        Verify TOTP
+      </h2>
+
+      <QRCode
+        value={props.qrCode}
+      />
+
+      <form
+        onSubmit={handleSubmit}
+      >
+        <label>
+          One-time password:
+
+          <input
+            type='text'
+            value={oneTimePassword}
+            onChange={handleChange}
+          />
+        </label>
+
+        <input
+          type='submit'
+          value='Verify'
+        />
+      </form>
+    </>
   );
 };
 
